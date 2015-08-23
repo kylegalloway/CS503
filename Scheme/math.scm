d712d889a243566cecfc4ca7729687667ce88e22
Kyle Galloway <kyle.galloway.1993@gmail.com>
2015-08-22 23:41:14 -0500

    Fixes log, adds comb

--- Scheme/math.scm
+++ Scheme/math.scm
@@ -1,9 +1,21 @@
-(define (power m n)
+(define (power m n) ; m raised to nth power
         (if (= n 1) m
             (* m (power m (- n 1)))
         )
 )
 
-(define (mod a b) (* (- a (/ a b) b)))
-(define (log m q) (/ (- q (mod q m)) m))
-; small problem with log
+(define (log m q) ; log base m of q
+        (if (= m q) 1
+            (+ 1 (log m (/ q m)))
+        )
+)
+
+(define (fact a) ; returns a!
+        (if (= a 0) 1
+            (+ a (fact (- a 1)))
+        )
+)
+
+(define (comb n k) ; returns n choose k
+        (/ (fact n) (* (fact k) (fact (- n k))))
+)
