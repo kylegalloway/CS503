4981c043398e009dbc0ab658cfae5c03c9df2bbc
Kyle Galloway <kyle.galloway.1993@gmail.com>
2015-09-09 14:50:38 -0500

    Test Prep

--- Scheme/functions.scm
+++ Scheme/functions.scm
@@ -84,14 +84,108 @@
         )
 )
 
+; MergeSort
+
+(define (split L L1 L2)
+        (if(null? L) (list L1 L2)
+           (if (null? (cdr L1)) (list(cons (car L) L1) L2)
+               (split (cddr L) (cons (car L) L1)
+                              (cons (cadr L) L2)
+               )
+           )
+        )
+)
+(define (merge L)
+        (if (null? L) L
+            (let ((L1 (car L))
+                  (L2 (cadr L))
+                 )
+                 (cond ((null? L1) L2)
+                       ((null? L2) L1)
+                       ((< (car L1) (car L2)
+                        (cons (car L1) (merge (list (cdr L1) L2))))
+                       (#t (cons (car L2) (merge (list L1 (cdr L2)))))
+                       )
+                 )
+            )
+        )
+)
+(define (mergesort L)
+        (if (or (null? L) (null? (cdr L))) L
+            (merge (map mergesort (split L '() '())))
+        )
+)
+
+
+; Selection Sort
+(define (selection_sort L)
+        (cond ((null? L) L)
+             (#t cons (smallest L (car L)) (selection_sort (remove L (smallest L (car L))))
+             )
+        )
+)
+
+(define (smallest L small)
+        (if (null? L) small
+            (if (< (car L) small) (smallest (cdr L) (car L))
+                (smallest (cdr L) small))
+        )
+)
+
+(define (remove L element)
+        (if (null? L)
+            (if (= (car L) element) (cdr L)
+                (cons (car L) (remove (cdr L) element))
+            )
+        )
+)
+
+; Quicksort
+(define (quicksort L)
+        (if (null? L) L
+            ((let (pivot (car L)))
+             (append (append (quicksort (filter (<= pivot)(cdr L)))(pivot)
+                     (quicksort (filter (> pivot) (cdr L)))
+                     )
+             )
+            )
+        )
+)
+
 ;-------------- Trees ----------------;
 ; Binary Search Trees
 
+; Better search
+(define (member? x T)
+        (if (null? T) #f
+            (cond ((< x (car T) (member? x (left T)))
+                  ((> x (car T) (member? x (right T)))
+                  ((= x (car T) #t)
+                  (#t #f))))
+            )
+        )
+)
+
+; Insertion
+(define (insert x T)
+        (if (null? T) (list x nil nil)
+            (cond ((< x (car T)) (list (car T)
+                                       (insert x (left T))
+                                       (right T))
+                  )
+                  ((> x (car T)) (list (car T)
+                                       (left T)
+                                       (insert x (right T)))
+                  )
+            )
+        )
+)
+
 ; Binary Search Tree Left
-(define (left T) (car (cdr T)))
+(define (left T) (cadr T))
 
 ; Binary Search Tree Right
-(define (right T) (car (cdr (cdr T))))
+(define (right T) (caddr T))
 
 ; Binary Search Tree Search (returns boolean)
 (define (search value T) ; T is a BST
